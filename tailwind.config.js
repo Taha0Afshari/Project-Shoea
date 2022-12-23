a304b9ad3831297eeb262d154d1aab7b64e0272c
Mohammadali Tamanaee <alitaman200041@gmail.com>
2022-12-23 14:14:37 +0330

    'first'

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    backgroundImage: {
+      mainOne: "url('/Project-Shoea/assets/img/WallpaperDog-20534610 1.png')",
+    },
   },
   plugins: [],
-}
+};
